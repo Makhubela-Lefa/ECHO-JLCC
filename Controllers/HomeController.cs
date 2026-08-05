bb668dfcdcb1cd152afda15752aa9cb7bececfc9
Makhubela-Lefa <makhubelalefa@gmail.com>
2026-08-05 20:46:48 +0200

    Create About ECHO page and update shared navigation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Echo.Web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
