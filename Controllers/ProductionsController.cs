670f640b885ae6cf235eda4817247c828be0a203
Makhubela-Lefa <makhubelalefa@gmail.com>
2026-08-05 20:21:04 +0200

    Create ECHO website landing page and production showcase

---
+++ Controllers/ProductionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Echo.Web.Controllers
+{
+    public class ProductionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WhenFireFell()
+        {
+            return View();
+        }
+    }
+}
