90adb1d4fc95a15771d75fcc6d74c3dad300cebe
Makhubela-Lefa <makhubelalefa@gmail.com>
2026-09-08 23:22:34 +0200

    Add protected admin dashboard and navigation structure

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Echo.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Echo.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +11,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -41,8 +46,22 @@ app.UseStatusCodePagesWithReExecute(
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    await IdentitySeeder.SeedRolesAsync(
+     services,
+     app.Configuration
+ );
+}
 
 app.Run();
---
+++ Services/IdentitySeeder.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Echo.Web.Services
+{
+    public static class IdentitySeeder
+    {
+        public static async Task SeedRolesAsync(
+            IServiceProvider serviceProvider,
+            IConfiguration configuration)
+        {
+            var roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var userManager =
+                serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            const string adminRole = "Admin";
+
+            if (!await roleManager.RoleExistsAsync(adminRole))
+            {
+                await roleManager.CreateAsync(
+                    new IdentityRole(adminRole)
+                );
+            }
+
+            var adminEmail =
+                configuration["AdminUser:Email"];
+
+            if (string.IsNullOrWhiteSpace(adminEmail))
+            {
+                return;
+            }
+
+            var adminUser =
+                await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser != null &&
+                !await userManager.IsInRoleAsync(adminUser, adminRole))
+            {
+                await userManager.AddToRoleAsync(
+                    adminUser,
+                    adminRole
+                );
+            }
+        }
+    }
+}
